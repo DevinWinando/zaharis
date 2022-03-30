ed6366fbbba739264f0c8713c3d6785dc0589520
Devin Winando <devin.winando13@gmail.com>
2022-03-30 18:10:06 +0700

    add: migration

--- database/migrations/2022_03_30_110820_create_reservasi_table.php
+++ database/migrations/2022_03_30_110820_create_reservasi_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateReservasiTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reservasi', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('password');
+            $table->date('mulai');
+            $table->date('selesai');
+            $table->string('token');
+            $table->foreignId('id_client')->constrained('client');
+            $table->foreignId('id_kamar')->constrained('kamar');
             $table->timestamps();
         });
     }
@@ -28,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reservasi');
     }
 }
