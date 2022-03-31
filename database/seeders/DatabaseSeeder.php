68ac8eab8907d17cba09f9bde598b8d23c4e700a
Devin Winando <devin.winando13@gmail.com>
2022-03-31 08:59:01 +0700

    fix: migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,20 @@ class DatabaseSeeder extends Seeder
             ['username' => 'admin', 'password' => Hash::make('admin'), 'role' => 'admin'],
             ['username' => 'resepsionis', 'password' => Hash::make('resepsionis'), 'role' => 'resepsionis'],
         ]);
+
+        DB::table('tipe_kamar')->insert([
+            ['nama' => 'Standard Room', 'harga' => 300000],
+            ['nama' => 'Deluxe Room', 'harga' => 500000],
+            ['nama' => 'Suite Room', 'harga' => 1000000],
+        ]);
+
+        DB::table('kamar')->insert([
+            ['nomor' => 101, 'deskripsi' => 'Ruangan ber-ac, kamar mandi di dalam, double bed, kapasitas 2 orang', 'id_tipe' => 1, 'dipesan' => 0, 'gambar' => '1648513012001_small-hotel-room-interior-with-double-bed-bathroom (1).webp'],
+            ['nomor' => 102, 'deskripsi' => 'Ruangan ber-ac, kamar mandi di dalam, double bed, kapasitas 2 orang', 'id_tipe' => 1, 'dipesan' => 0, 'gambar' => '1648513012002_small-hotel-room-interior-with-double-bed-bathroom (1).webp'],
+            ['nomor' => 103, 'deskripsi' => 'Ruangan ber-ac, kamar mandi di dalam, double bed, kapasitas 2 orang', 'id_tipe' => 1, 'dipesan' => 0, 'gambar' => '1648513012003_small-hotel-room-interior-with-double-bed-bathroom (1).webp'],
+            ['nomor' => 104, 'deskripsi' => 'Ruangan ber-ac, kamar mandi di dalam, double bed, kapasitas 2 orang', 'id_tipe' => 1, 'dipesan' => 0, 'gambar' => '1648513012004_small-hotel-room-interior-with-double-bed-bathroom (1).webp'],
+            ['nomor' => 201, 'deskripsi' => 'Ruangan ber-ac, kamar mandi di dalam, double bed, kapasitas 2 orang, sofa, televisi', 'id_tipe' => 2, 'gambar' => '1648513316001_cozy-studio-apartment-with-bedroom-living-space (1).webp'],
+            ['nomor' => 202, 'deskripsi' => 'Ruangan ber-ac, kamar mandi di dalam, double bed, kapasitas 2 orang, sofa, televisi', 'id_tipe' => 2, 'gambar' => '1648513316002_cozy-studio-apartment-with-bedroom-living-space (1).webp'],
+        ]);
     }
 }
